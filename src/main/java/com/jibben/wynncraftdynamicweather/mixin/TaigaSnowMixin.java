cefd6e07bac6f4c1f4479194a12ebaf026795ad4
Elijah Jibben <quikjay360@gmail.com>
2024-08-02 22:22:42 -0700

    Improved compatibility

--- src/main/java/com/jibben/wynncraftdynamicweather/mixin/TaigaSnowMixin.java
+++ src/main/java/com/jibben/wynncraftdynamicweather/mixin/TaigaSnowMixin.java
@@ -1,24 +1,22 @@
 package com.jibben.wynncraftdynamicweather.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.world.ClientWorld;
 import net.minecraft.registry.tag.BiomeTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
 
 @Mixin(Biome.class)
 public class TaigaSnowMixin {
-    @Inject(method = "getPrecipitation", at = @At("HEAD"), cancellable = true)
-    private void getPrecipitation(BlockPos pos, CallbackInfoReturnable<Biome.Precipitation> cir) {
-        MinecraftClient client = MinecraftClient.getInstance();
-
-        if (client.world.getBiome(pos).isIn(BiomeTags.IS_TAIGA)) {
-            cir.setReturnValue(Biome.Precipitation.SNOW);
+    @ModifyReturnValue(method = "getPrecipitation", at = @At("RETURN"))
+    private Biome.Precipitation getPrecipitation(Biome.Precipitation original, BlockPos pos) {
+        if (MinecraftClient.getInstance().world.getBiome(pos).isIn(BiomeTags.IS_TAIGA)) {
+            return Biome.Precipitation.SNOW;
         }
-
+        return original;
     }
+
 }
